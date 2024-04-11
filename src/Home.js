b3b7f8a0c1ac5f13f9d27f2f96f214624167f22a
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-11 23:15:24 +0530

    custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,14 @@
-import { useState, useEffect } from "react";
 import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
 
-const Home = () => {
+const Home = () => { 
+    const {data, isLoading, error} = useFetch("http://localhost:8000/blogs");
     // eslint-disable-next-line no-unused-vars
-    const [blogs, setBlogs] = useState(null)
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(res => {
-            if (!res.ok) {
-                throw Error('Could not fetch data for the resource!')
-            }
-        return res.json();
-    })
-        .then((data) => {
-            setBlogs(data); 
-            setIsLoading(false)})
-            .catch((error) => {
-                setError(error.message);
-                setIsLoading(false);
-            })
-    },[]);
-
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
-            {blogs && <Bloglist blogs={blogs} title="All Blogs"/>}
+            {data && <Bloglist blogs={data} title="All Blogs"/>}
         </div> 
     );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,28 @@
+ import { useState, useEffect } from 'react';
+
+ const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+        .then(res => {
+            if (!res.ok) {
+                throw Error('Could not fetch data for the resource!')
+            }
+        return res.json();
+    })
+        .then((data) => {
+            setData(data); 
+            setIsLoading(false)})
+            .catch((error) => {
+                setError(error.message);
+                setIsLoading(false);
+            })
+    },[url]);
+
+    return { data, isLoading, error};
+ }
+
+ export default useFetch
