86e3f408d5137e77cac485fa54c903bfddcce1c9
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-11 23:02:52 +0530

    New Commit

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,30 @@ import Bloglist from "./Bloglist";
 const Home = () => {
     // eslint-disable-next-line no-unused-vars
     const [blogs, setBlogs] = useState(null)
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
         .then(res => {
+            if (!res.ok) {
+                throw Error('Could not fetch data for the resource!')
+            }
         return res.json();
     })
-        .then((data) => {setBlogs(data)})
+        .then((data) => {
+            setBlogs(data); 
+            setIsLoading(false)})
+            .catch((error) => {
+                setError(error.message);
+                setIsLoading(false);
+            })
     },[]);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
             {blogs && <Bloglist blogs={blogs} title="All Blogs"/>}
         </div> 
     );
