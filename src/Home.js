a1d7b8e879e45533292b0cc403fc101d17b9c490
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-11 16:31:38 +0530

    Changes for useState()

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {
+            title: 'My New Website', body: 'lorem ipsum.....', id: 1
+        },
+        {
+            title: 'Welcome Party', body: 'lorem ipsum......', id: 2
+        }
+    ])
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={ blog.id }>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.body}</p>
+                </div>
+            ))}
         </div> 
     );
 }
- 
+
 export default Home;
