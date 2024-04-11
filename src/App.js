96b11eaf9395ac61d2a9a158ad9476bcec05bda7
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-12 00:11:04 +0530

    useParams() added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create"
-
+import BlogDetails from "./blogDetails";
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
@@ -16,6 +16,9 @@ function App() {
               </Route>
               <Route path="/create">
                 <Create />
+              </Route>
+              <Route path="/blogs/:id">
+                <BlogDetails />  
               </Route>
             </Switch>
           </div>
