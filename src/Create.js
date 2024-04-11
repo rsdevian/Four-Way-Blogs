cd9e396e57859cf02bc414e65b3c373a70f2489e
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-12 00:28:48 +0530

    New Css Added

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,19 @@
 const Create = () => {
     return ( 
         <div className="create">
-            <h2>Create Compoent</h2>
+            <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title: </label>
+                <input type="text" required/>
+                <label>Blog Body:</label>
+                <textarea required></textarea>
+                <label>Blog Author: </label>
+                <select>
+                    <option value="Mario">Mario</option>
+                    <option value="Liugi">Liugi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
