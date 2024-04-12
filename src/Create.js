4f99db8cbb8734e14242261c2baf700553ba0450
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-12 13:48:11 +0530

    Create Blog Page Added

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        } )
+    }
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title: </label>
-                <input type="text" required/>
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
                 <label>Blog Body:</label>
-                <textarea required></textarea>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
                 <label>Blog Author: </label>
-                <select>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
                     <option value="Mario">Mario</option>
                     <option value="Liugi">Liugi</option>
                 </select>
@@ -17,5 +33,5 @@ const Create = () => {
         </div>
      );
 }
- 
+
 export default Create;
